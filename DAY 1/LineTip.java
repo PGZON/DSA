5550aefbdb8abc761d07daf57802d993facb2ad9
PGZON <prathameshgadgil2005@gmail.com>
2025-04-03 16:17:20 +0530

    Day 2

--- DAY 1/LineTip.java
+++ DAY 1/LineTip.java
@@ -5,6 +5,7 @@ public class LineTip {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+        
 
         while (t -->0){
             int n = sc.nextInt();
--- DAY 2/QuickShort.java
+++ DAY 2/QuickShort.java
@@ -1,12 +1,18 @@
-public class Pivot_Target {
+public class QuickShort {
     public static void main(String[] args) {
-        int arr[] = {  3, 0, 9, 6, 7,8 };
-        int ans = pivot(arr, 0, arr.length - 1);
-        System.out.println(ans);
+        int arr[] = {8,3,2,4,6,5,8,0};
+        quicksort(arr, 0, arr.length-1);
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + "\t");
+            System.out.print(arr[i]+"\t");
         }
 
+    }
+
+    public static void quicksort (int arr[] , int si, int ei){
+        if(si>ei) return;
+        int pi = pivot(arr, si, ei);
+        quicksort(arr, si, pi-1);//left side divide
+       quicksort(arr, pi+1, ei);//right side divide
     }
 
     public static int pivot(int arr[],int si,int ei){
